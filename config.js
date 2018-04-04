9efb4018613a7e39adfd004aa403ba6f50bf4ae6
Harishankar Nagarajan <Harish@Harishankars-MacBook-Pro-2.local>
2018-04-03 23:13:32 -0400

    config

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+// This file contains private configuration details.
+// Do not add it to your Git repository.
+module.exports = {
+  "mongodbHost" : "mongodb://heroku_j54gzxv4:bqb4jpoeqjgeesds59iuco23ok@ds231199.mlab.com:31199/heroku_j54gzxv4"
+};
